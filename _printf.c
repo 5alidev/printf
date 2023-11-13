771a4d19ca73cedadca1d0a09a7a847f952cee7a
Youssef <spanya2017@gmail.com>
2023-11-13 03:34:05 -0800

    setup project files

---
+++ _printf.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int _printf(const char *format, ...)
+{
+}
---
+++ main.h
@@ -0,0 +1,6 @@
+#ifndef _main_h
+#define _main_h
+#include <stdio.h>
+#include <stdarg.h>
+int _printf(const char *format, ...);
+#endif
