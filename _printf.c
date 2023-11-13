3b0d4b26639126a824f37c9afc35f9efd81c6876
5alidev <khalidbenhamou.dev@outlook.com>
2023-11-13 05:49:03 -0800

    printf with c, s and %

--- _printf.c
+++ _printf.c
@@ -1,9 +1,54 @@
 #include "main.h"
 /**
- * main - Entry point
+ * _printf - function that produces output according to a format.
  *
- * Return: Always 0
+ * @format: format of value to print
+ *
+ * Return:length of value
  */
 int _printf(const char *format, ...)
 {
+	char chiHaja;
+	int i = 0, count = 0;
+	int formatLen = strlen(format);
+	char *str = NULL;
+	va_list args;
+
+	va_start(args, format);
+	while (i < formatLen)
+	{
+		count++;
+		if (format[i] == '%')
+		{
+			switch (format[i + 1])
+			{
+				case 'c':
+					chiHaja = (char)va_arg(args, int);
+					write(1, &chiHaja, 1);
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (str == NULL)
+						str = "(null)";
+					while (*str)
+					{
+						write(1, str, 1);
+						str++;
+						count++;
+					}
+					break;
+				case '%':
+					write(1, "%", 1);
+					break;
+			}
+			i++;
+		}
+		else
+		{
+			write(1, &format[i], 1);
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
 }
--- main.h
+++ main.h
@@ -2,5 +2,8 @@
 #define _main_h
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
 int _printf(const char *format, ...);
 #endif
